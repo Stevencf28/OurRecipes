41f8f1ca3e661eb19ca205ac36229e958874640a
schen196 <84357143+schen196@users.noreply.github.com>
2022-03-22 17:48:50 -0400

    16 create UI for recipe page (#70)
    * partial ui completed
    
    * small changes, committed due to changing workspace
    
    * completed prototype of recipe details
    
    * Completed Prototype

---
+++ app/routes/recipe/$name.tsx
@@ -0,0 +1,142 @@
+// preparation for loading data into the page
+//import type { LoaderFunction } from "remix";
+
+import {
+  Badge,
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Stack,
+} from "react-bootstrap";
+import { Image } from "remix-image";
+
+/*
+// preparation for loading data into the page for the person creating the response
+export const loader: LoaderFunction = ({ params }) => {
+  return (
+
+  )
+}
+*/
+export default function name() {
+  return (
+    <Stack gap={2} direction="vertical">
+      {/* Here is the title for the page */}
+      <head>
+        <title>Recipe Details</title>
+      </head>
+      {/*Recipe Title*/}
+      <div className="d-flex justify-content-center">
+        <h1 id="recipeTitle">Recipe Title</h1>
+      </div>
+
+      {/*this <Image /> tag is from remix-image
+      it's easier to use to add in responsive images
+      Here's a website that shows its usage
+      https://bestofreactjs.com/repo/Josh-McFarlin-remix-image
+      I recommend to find other sources if you don't understand this part*/}
+      <div className="d-flex justify-content-center">
+        <Image />
+      </div>
+
+      <div>
+        {/*Cooking Tools and Cooking Time Container*/}
+        <Container>
+          <ListGroup as="ol" className="d-flex" variant="flush">
+            <ListGroup.Item as="li">
+              <div className="fw-bold">Cooking Tools</div>
+              <ListGroup
+                className="d-flex justify-content-between align-items-start"
+                id="cookingToolsGroup"
+              >
+                {/*Cooking Tools*/}
+                {/*For the person creating the response,
+                  make it so that it will create the multiple listgroup.items
+                  so that it is more adaptive and does not
+                  require the amount of items to be hardcoded
+                  */}
+                <ListGroup.Item
+                  as="li"
+                  className="d-flex justify-content-around align-items-start"
+                >
+                  <div className="me-auto">
+                    <div className="fw-bold">Cooking Tool</div>
+                  </div>
+                  <Badge bg="primary" id="amount" pill>
+                    Amount
+                  </Badge>
+                </ListGroup.Item>
+              </ListGroup>
+            </ListGroup.Item>
+            <ListGroup.Item
+              as="li"
+              className="d-flex justify-content-between align-items-start"
+            >
+              <div className="d-flex">
+                <div className="mr-auto p-2 fw-bold">Cooking Time</div>
+              </div>
+              <div className="d-flex">
+                <Badge
+                  bg="primary"
+                  className="align-self-end"
+                  id="cookingTime"
+                  pill
+                >
+                  x minutes/hours
+                </Badge>
+              </div>
+            </ListGroup.Item>
+            <ListGroup.Item as="li">
+              <div className="fw-bold">Ingredients</div>
+              <ListGroup
+                className="d-flex justify-content-between align-items-start"
+                id="ingredientsGroup"
+              >
+                {/*Ingredients*/}
+                {/*For the person creating the response,
+                  make it so that it will create the multiple listgroup.items
+                  so that it is more adaptive and does not
+                  require the amount of items to be hardcoded
+                  */}
+                <ListGroup.Item
+                  as="li"
+                  className="d-flex justify-content-around align-items-start"
+                >
+                  <div className="me-auto">
+                    <div className="fw-bold">Ingredient</div>
+                  </div>
+                  <Badge bg="primary" id="amount" pill>
+                    Amount
+                  </Badge>
+                </ListGroup.Item>
+              </ListGroup>
+            </ListGroup.Item>
+            <ListGroup.Item as="li">
+              <div className="fw-bold">Instructions</div>
+              <ListGroup
+                className="d-flex justify-content-between align-items-start"
+                id="instructionsGroup"
+              >
+                {/*Ingredients*/}
+                {/*For the person creating the response,
+                  make it so that it will create the multiple listgroup.items
+                  so that it is more adaptive and does not
+                  require the amount of items to be hardcoded
+                  */}
+                <ListGroup.Item
+                  as="li"
+                  className="d-flex justify-content-around align-items-start"
+                >
+                  <div className="me-auto">
+                    <div className="fw-bold">Step x</div>
+                    <p>Enter Description Here</p>
+                  </div>
+                </ListGroup.Item>
+              </ListGroup>
+            </ListGroup.Item>
+          </ListGroup>
+        </Container>
+      </div>
+    </Stack>
+  );
+}
