7fd5a2ef4d21d6de329e44ef3eb11d4edf55278b
Jamee Kim <jameekim@nodaplife.me>
2022-04-08 15:52:17 -0400

    #106 Add DB model for collections (#134)

---
+++ app/models/Collection.server.ts
@@ -0,0 +1,70 @@
+/**
+ * MongoDB collection for the recipe collections
+ *
+ * While we can make the recipe collections into a subdocument of {@link User}
+ * documents, making a separate document is more resilient to future changes.
+ * For example, if we were to implement the share functionality for recipe
+ * collections, it is better to have an id ready for each collection that is
+ * guaranteed to be unique and easy to query from the database (i.e. indexed).
+ * It is also easier to have permission control settings with a separate
+ * document for recipe collections.
+ */
+
+import mongoose from "mongoose";
+
+/**
+ * Structure of the collection model
+ */
+export interface CollectionData {
+  user: mongoose.Types.ObjectId;
+  name: string; // The default collection name will be an empty string
+  recipes: number[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const CollectionSchema = new mongoose.Schema<CollectionData>(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: "user",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    recipes: [
+      {
+        type: Number,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// The combination of user and name should be unique
+CollectionSchema.index({ user: 1, name: 1 }, { unique: true });
+
+const modelName = "collection";
+
+// Remove the previous version of the model that is already registered. While
+// inefficient, this ensures that any changes made to the models are applied.
+if (process.env.NODE_ENV !== "production" && mongoose.models[modelName]) {
+  delete mongoose.models[modelName];
+}
+
+/**
+ * Mongoose model for recipe collections
+ */
+const Collection = mongoose.model(modelName, CollectionSchema);
+export default Collection;
+
+/**
+ * The type of Mongoose model for recipe collections
+ */
+export type CollectionDoc = mongoose.HydratedDocument<CollectionData>;
--- app/models/User.server.ts
+++ app/models/User.server.ts
@@ -1,4 +1,6 @@
+import { ClientSession } from "mongodb";
 import mongoose from "mongoose";
+import Collection, { CollectionDoc } from "./Collection.server";
 
 /**
  * Structure of the user model
@@ -9,7 +11,27 @@ export interface UserData {
   displayName?: string;
 }
 
-const UserSchema = new mongoose.Schema<UserData>({
+export interface UserInstanceMethods {
+  /**
+   * Get the list of all collections this user has, sorted by the name
+   */
+  collections: () => Promise<CollectionDoc[]>;
+
+  /**
+   * Get the default collection, creating one if it doesn't exist
+   *
+   * You can opt-in to use a session if you want to use a transaction.
+   */
+  defaultCollection: (session?: ClientSession) => Promise<CollectionDoc>;
+}
+
+type UserModelType = mongoose.Model<UserData, {}, UserInstanceMethods>;
+
+const UserSchema = new mongoose.Schema<
+  UserData,
+  UserModelType,
+  UserInstanceMethods
+>({
   email: {
     type: String,
     required: true,
@@ -24,6 +46,25 @@ const UserSchema = new mongoose.Schema<UserData>({
   },
 });
 
+UserSchema.methods.collections = async function (this: UserDoc) {
+  return await Collection.find(
+    { user: this._id },
+    {},
+    {
+      sort: { name: 1 },
+    },
+  );
+};
+
+UserSchema.methods.defaultCollection = async function (
+  this: UserDoc,
+  session?: ClientSession,
+) {
+  const data = { user: this._id, name: "" };
+  const collection = await Collection.findOne(data, {}, { session });
+  return collection ?? new Collection(data).save({ session });
+};
+
 const modelName = "user";
 
 // Remove the previous version of the model that is already registered. While
@@ -35,10 +76,10 @@ if (process.env.NODE_ENV !== "production" && mongoose.models[modelName]) {
 /**
  * Mongoose model for users
  */
-const User = mongoose.model(modelName, UserSchema);
+const User = mongoose.model<UserData, UserModelType>(modelName, UserSchema);
 export default User;
 
 /**
  * The type of mongoose document for users
  */
-export type UserDoc = mongoose.HydratedDocument<UserData>;
+export type UserDoc = mongoose.HydratedDocument<UserData, UserInstanceMethods>;
