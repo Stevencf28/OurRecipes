fb017901e118498679c9e9cdb371c61f31515f90
schen196 <84357143+schen196@users.noreply.github.com>
2022-04-12 18:59:17 -0400

    added response from server (#140)

--- app/routes/account/collections/$id.tsx
+++ app/routes/account/collections/$id.tsx
@@ -1,7 +1,40 @@
-import { useParams } from "remix";
+import { LoaderFunction, MetaFunction, json, useLoaderData } from "remix";
+import Collection from "~/models/Collection.server";
+
+interface CollectionInfo {
+  name: string;
+  recipes: number[];
+}
+
+interface LoaderData {
+  collection?: CollectionInfo | undefined;
+}
+
+export const meta: MetaFunction = ({ data }) => {
+  // Note: Do NOT remove that `?` for accessing the `collection` field from the data
+  // I'm not sure if the data returned by the loader is always guaranteed to be
+  // there. Don't get fooled by the `as LoaderData` part.
+  const title =
+    (data as LoaderData)?.collection?.name ?? "Collection Not Found";
+
+  return { title };
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  let collection: CollectionInfo | undefined;
+
+  if (params.id) {
+    collection = (await Collection.findById(params.id)) ?? undefined;
+  }
+  const status = collection ? 200 : 404;
+  return json<LoaderData>({ collection }, status);
+};
 
 export default function CollectionId(): JSX.Element {
-  const params = useParams();
-
-  return <>Collection page for {params.id}</>;
+  const data = useLoaderData<LoaderData>();
+  return (
+    <>
+      <h3>Collection page for {data.collection?.name}</h3>
+    </>
+  );
 }
